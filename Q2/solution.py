4cf211dd2e30dbbabb597d8cb9a56027337dac24
karenlky <karenlky1230@gmail.com>
2023-03-11 10:31:11 +0800

    Update Q3

--- Q2/solution.py
+++ Q2/solution.py
@@ -1,26 +1,29 @@
-rec = input()
-tmp = rec.split('\n')
-total_rec = int(tmp[0])
-rec_lines = []
-for x in range(1, total_rec+1):
-    rec_lines.append(tmp[x])
+def check_validity():
+    rec = input()
+    tmp = rec.split('\n')
+    total_rec = int(tmp[0])
+    rec_lines = []
+    for x in range(1, total_rec+1):
+        rec_lines.append(tmp[x])
 
-allValid = "true"
-errorCodes = []
-for record in rec_lines:
-    record_arr = record.split(" ")
-    isValid = record_arr[1]
+    allValid = "true"
+    errorCodes = []
+    for record in rec_lines:
+        record_arr = record.split(" ")
+        isValid = record_arr[1]
+        if allValid == "true":
+            allValid = isValid
+        if isValid != "true":
+            errorCodes.append(record_arr[2])
+
     if allValid == "true":
-        allValid = isValid
-    if isValid != "true":
-        errorCodes.append(record_arr[2])
-
-if allValid == "true":
-    print("Yes")
-else:
-    print("No")
-    errors = errorCodes[0]
-    for x in range(1, len(errorCodes)):
-        errors += " "
-        errors += errorCodes[x]
-    print(errors)
+        print("Yes")
+        return(True, [])
+    else:
+        print("No")
+        errors = errorCodes[0]
+        for x in range(1, len(errorCodes)):
+            errors += " "
+            errors += errorCodes[x]
+        print(errors)
+        return(False, errorCodes)
